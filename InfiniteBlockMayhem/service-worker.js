99569fe38890ca5d8a4d43e07f639af5ddc441c8
kenny zhang <kenny.zhang@gameofapps.org>
2020-08-28 11:44:40 -0700

    UX improvements, difficuly scaling and fixed favourites system bug

--- InfiniteBlockMayhem/service-worker.js
+++ InfiniteBlockMayhem/service-worker.js
@@ -44,7 +44,7 @@ self.addEventListener('fetch', (evt) => {
             .catch(() => {
                 return caches.open(CACHE_NAME)
                     .then((cache) => {
-                        return cache.match(evt.request);
+                        return cache.match('index.html');
                     });
             })
     );
