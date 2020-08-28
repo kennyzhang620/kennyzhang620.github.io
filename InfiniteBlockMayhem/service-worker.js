c23622a551f2b67374f847df3bb1a897b6a6feaf
kenny zhang <kenny.zhang@gameofapps.org>
2020-08-28 11:22:57 -0700

    UX improvements, difficuly scaling and fixed favourites system bug

--- InfiniteBlockMayhem/service-worker.js
+++ InfiniteBlockMayhem/service-worker.js
@@ -1,26 +1,27 @@
-﻿
+﻿'use strict';
+
 // CODELAB: Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v1';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-    '../InfiniteBlockMayhem/index.html',
-    '../InfiniteBlockMayhem/Build/sInfiniteBlockMayhem.data.unityweb',
-    '../InfiniteBlockMayhem/Build/InfiniteBlockMayhem.json',
-    '../InfiniteBlockMayhem/Build/InfiniteBlockMayhem.wasm.code.unityweb',
-    '../InfiniteBlockMayhem/Build/InfiniteBlockMayhem.wasm.framework.unityweb',
-    '../InfiniteBlockMayhem/Build/UnityLoader.js',
-    '../InfiniteBlockMayhem/TemplateData/favicon.ico',
-    '../InfiniteBlockMayhem/TemplateData/fullscreen.png',
-    '../InfiniteBlockMayhem/TemplateData/progressEmpty.Dark.png',
-    '../InfiniteBlockMayhem/TemplateData/progressEmpty.Light.png',
-    '../InfiniteBlockMayhem/TemplateData/progressFull.Dark.png',
-    '../InfiniteBlockMayhem/TemplateData/progressFull.Light.png',
-    '../InfiniteBlockMayhem/TemplateData/progressLogo.Dark.png',
-    '../InfiniteBlockMayhem/TemplateData/progressLogo.Light.png',
-    '../InfiniteBlockMayhem/TemplateData/style.css',
-    '../InfiniteBlockMayhem/TemplateData/UnityProgress.js',
-    '../InfiniteBlockMayhem/TemplateData/webgl-logo.png',
+    './index.html',
+    './Build/sInfiniteBlockMayhem.data.unityweb',
+    './Build/InfiniteBlockMayhem.json',
+    './Build/InfiniteBlockMayhem.wasm.code.unityweb',
+    './Build/InfiniteBlockMayhem.wasm.framework.unityweb',
+    './Build/UnityLoader.js',
+    './TemplateData/favicon.ico',
+    './TemplateData/fullscreen.png',
+    './TemplateData/progressEmpty.Dark.png',
+    './TemplateData/progressEmpty.Light.png',
+    './TemplateData/progressFull.Dark.png',
+    './TemplateData/progressFull.Light.png',
+    './TemplateData/progressLogo.Dark.png',
+    './TemplateData/progressLogo.Light.png',
+    './TemplateData/style.css',
+    './TemplateData/UnityProgress.js',
+    './TemplateData/webgl-logo.png',
 ];
 
 self.addEventListener('install', (evt) => {
