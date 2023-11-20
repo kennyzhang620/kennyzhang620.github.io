4b5f02264ed4ff2c236e08163a37953e4d68f52c
kenny zhang <ktzhang@sfu.ca>
2023-11-19 23:34:08 -0800

    add2

---
+++ P2-312A/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Backup-0.1";
+const contentToCache = [
+    "Build/P2-312A.loader.js",
+    "Build/P2-312A.framework.js.unityweb",
+    "Build/P2-312A.data.unityweb",
+    "Build/P2-312A.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
