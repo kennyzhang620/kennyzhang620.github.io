ff722fa01dfbd6f4b62cf214739447bfd8a04d18
KTZHANG <ktzhang@sfu.ca>
2025-06-19 19:27:27 -0700

    holy fuck:

---
+++ testweb/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "simonfraser-Bee Explorer!-3.12.0";
+const contentToCache = [
+    "Build/testweb.loader.js",
+    "Build/testweb.framework.js.unityweb",
+    "Build/testweb.data.unityweb",
+    "Build/testweb.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
